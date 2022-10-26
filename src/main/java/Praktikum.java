b38298d2549d26f1414831108ed0ce17d57fa0cf
Darina Vorob'eva <darishka@bk.ru>
2022-10-26 13:40:03 +0300

    add 2 tests

---
+++ src/main/java/Praktikum.java
@@ -0,0 +1,37 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.junit.Test;
+import org.junit.After;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class Praktikum {
+
+    private WebDriver driver;
+
+    @Test
+    public void test() {
+        // Создаём драйвер для браузера Chrome
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--no-sandbox", "--headless", "--disable-dev-shm-usage");
+        driver = new ChromeDriver(options);
+        driver.get("https://qa-mesto.praktikum-services.ru/");
+        driver.findElement(By.id("email")).sendKeys("darishka@bk.ru");
+        driver.findElement(By.id("password")).sendKeys("SkyPro2021");
+        driver.findElement(By.className("auth-form__button")).click();
+        new WebDriverWait(driver, 3)
+                .until(ExpectedConditions.visibilityOfElementLocated(By.className("places__list")));
+        WebElement element = driver.findElement(By.cssSelector(".places__item"));
+        ((JavascriptExecutor)driver).executeScript("arguments[0].scrollIntoView();", element);
+    }
+
+    @After
+    public void teardown() {
+        driver.quit();
+    }
+}
+
